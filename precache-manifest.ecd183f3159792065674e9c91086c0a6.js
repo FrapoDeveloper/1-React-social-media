468fdee80e3edb68b7afd2481c4969ef6e0835b2
Fraporitmos <fran_espino189@outlook.com>
2020-10-23 11:54:50 -0500

    Updates

---
+++ precache-manifest.ecd183f3159792065674e9c91086c0a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c9d5574e7569e48bface09fa597a75",
+    "url": "/1-React-social-media/index.html"
+  },
+  {
+    "revision": "7a7ffbe881b84ebd65ac",
+    "url": "/1-React-social-media/static/css/main.092ac23c.chunk.css"
+  },
+  {
+    "revision": "0b352b242fb37aae465a",
+    "url": "/1-React-social-media/static/js/2.15863750.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/1-React-social-media/static/js/2.15863750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7ffbe881b84ebd65ac",
+    "url": "/1-React-social-media/static/js/main.dbd7cff3.chunk.js"
+  },
+  {
+    "revision": "c490ee4d9f6a7ac87368",
+    "url": "/1-React-social-media/static/js/runtime-main.04744e04.js"
+  },
+  {
+    "revision": "4a8c23476a7c20c5bee2a752a6f96e9e",
+    "url": "/1-React-social-media/static/media/instagram.4a8c2347.png"
+  },
+  {
+    "revision": "8f35a40403a84631c4125c4f1859c7a6",
+    "url": "/1-React-social-media/static/media/twitter.8f35a404.png"
+  }
+]);
